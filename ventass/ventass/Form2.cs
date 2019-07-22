a98715489749fc7bbbaeab353325234d303be18c
Elizabeth-Ramirez <ramirez.elizabethg430@gmail.com>
2019-07-22 16:44:55 -0600

    creando clases

---
+++ ventass/ventass/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ventass
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cliente1 = new Cliente ();
+            cliente1.Id = 1;
+            cliente1.Nombre  = "Elizabeth Ramirez";
+            cliente1.Telefono = "9874-5123";
+            cliente1.Ciudad  = "El Progreso";
+
+            Cliente  cliente2 = new Cliente();
+            cliente2.Id = 2;
+            cliente2.Nombre  = "Ana Moreno";
+            cliente2.Telefono = "9895-7477";
+            cliente2.Ciudad = "El Progreso";
+
+            Cliente cliente3 = new Cliente();
+            cliente3.Id = 2;
+            cliente3.Nombre = "Karol Ramirez";
+            cliente3.Telefono = "9696-4252";
+            cliente3.Ciudad = "Comayagua";
+
+
+ 
+          
+
+        }
+    }
+}
