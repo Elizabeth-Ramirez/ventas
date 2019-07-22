1bf079c53a40dcd1a3d13fd501a975652b7045ea
Elizabeth-Ramirez <ramirez.elizabethg430@gmail.com>
2019-07-22 17:02:25 -0600

    terminado

--- ventass/ventass/Form2.cs
+++ ventass/ventass/Form2.cs
@@ -23,31 +23,26 @@ namespace ventass
             ciudad1.Id = 1;
             ciudad1.Descripcion = "San Pedro";
 
-            var ciudad2 = new Ciudad();
-            ciudad1.Id = 1;
-            ciudad1.Descripcion = "El Progreso";
-
-            var ciudad3 = new Ciudad();
-            ciudad1.Id = 1;
-            ciudad1.Descripcion = "Comayagua";
+          
 
             var cliente1 = new Cliente ();
             cliente1.Id = 1;
             cliente1.Nombre  = "Elizabeth Ramirez";
             cliente1.Telefono = "9874-5123";
-           
+            cliente1.Ciudad = ciudad1;
+
 
             Cliente  cliente2 = new Cliente();
             cliente2.Id = 2;
             cliente2.Nombre  = "Ana Moreno";
             cliente2.Telefono = "9895-7477";
-            
+            cliente2.Ciudad = ciudad1;
 
             Cliente cliente3 = new Cliente();
             cliente3.Id = 2;
             cliente3.Nombre = "Karol Ramirez";
             cliente3.Telefono = "9696-4252";
-          
+            cliente3.Ciudad = ciudad1;
 
 
             var listaclientes = new List<Cliente>();
@@ -57,7 +52,7 @@ namespace ventass
 
             foreach (var cliente in listaclientes )
             {
-                MessageBox.Show(cliente.Id + " " + cliente.Nombre + " " + cliente.Telefono + " " + cliente.Ciudad);
+                MessageBox.Show(cliente.Id + " " + cliente.Nombre + " " + cliente.Telefono + " " + cliente.Ciudad.Descripcion);
             }
 
         }
